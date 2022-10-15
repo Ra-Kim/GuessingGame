1cad37abc916469d8edb57d45f435ce878affacf
Alaede Increase <78408517+Ra-Kim@users.noreply.github.com>
2022-10-15 16:42:18 +0000

    Fixed some bugs in the code. Enjoy

--- index.js
+++ index.js
@@ -8,20 +8,33 @@ function guessTheNumber (range) {
   number = Number(prompt(
     `Guess a number among 1 ${(range < 3) ? "and" : "to" } ${range} : `
   ))
-  
+  let points = range - 1
   if( number === variable) {
-    points = range - 1
     console.log(`Correct, you have ${points} point${points=== 1 ? "" : "s"}`)
   } else {
-    correct = false
-    console.log(`Wrong, your score is ${points?points:0}`)
-    console.log("New Game")
-    guessTheNumber(range = 2)
+    correct=false
+    console.log(`Wrong, your score is ${points-1}`)
+    console.log("Do you want to play again?")
+    let decision = prompt("Enter Y/N")
+    if(decision.toLowerCase() == "y"){
+      correct=true;
+      range = 2
+      game(range);
+    }else if(decision.toLowerCase() == "n"){
+      console.log("Thanks for playing")
+    }else{
+      console.log("Enter a correct letter")
+    }
+    
   }
   
 }
-
-while(correct){
-  guessTheNumber(range)
-  range++
+function game(range = 2){
+    while(correct){
+    guessTheNumber(range)
+    range++
+  }
 }
+
+game()
+
